a43aada85cc5b9b885736343fda0e1813185399c
aakashdeshamne <21512493.dypit@dypvp.edu.in>
2025-01-04 01:01:44 +0530

    third commit

--- DataBase/Connetion.cjs
+++ DataBase/Connetion.cjs
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://127.0.0.1:27017/LibraryManagementSystem';
+const url = 'mongodb+srv://deshmaneakash100:<aakash2003>@cluster4.y4ofk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster4';
 
 mongoose.connect(url).then(() => {
     console.log('Connection successful');
